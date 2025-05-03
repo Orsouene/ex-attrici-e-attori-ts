086d5b922c4a1fa4b739f8c9c540a4f0b681b7a6
Orsouene <orsoueneelaouizeb@gmail.com>
2025-05-03 10:20:30 +0200

    MileStone-1

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,10 @@
-console.log("test")
 
+//! MileStone-1
+type Person = {
+  readonly id:number;
+  readonly name : string;
+  birth_year:number;
+  death_year?:number;
+  biography:string;
+  image:string;
+};
